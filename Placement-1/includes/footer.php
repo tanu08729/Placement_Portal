8c47ae046aea73859bda4c708fe7429f67312743
Tanu Singh Jadaun <tanu.jadaun_cs19@gla.ac.in>
2022-06-02 12:14:20 +0530

    first commit

---
+++ Placement-1/includes/footer.php
@@ -0,0 +1,49 @@
+<!--/footer-->
+<div class="footer">
+    <div class="footer-top">
+        <div class="col-md-4 footer-grid">
+            <h4>PLACEMENT Management System</h4>
+            <ul class="bottom">
+                <li>Designed By: Shubhashi, Tanu, Samradhi</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>Message Us Now</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-home"></i>Available 24/7 </li>
+                <li><i class="glyphicon glyphicon-envelope"></i><a href="Shubhashi.chaudhary_cs19@gla.ac.in">Shubhashi.chaudhary_cs19@gla.ac.in</a></li>
+                <li><i class="glyphicon glyphicon-envelope"></i>+91 7525874871</li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            <h4>Address Location</h4>
+            <ul class="bottom">
+                <li><i class="glyphicon glyphicon-map-marker"></i>GLA University Mathura.</li>
+                <li><i class="glyphicon glyphicon-earphone"></i>Mobile: (+639) 272777334 </li>
+            </ul>
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>
+<div class="copy">
+    <p>&copy; 2022 Placement Management System All Rights Reserved</p>
+</div>
+<div class="clearfix"> </div>
+<!--//footer-->
+<!--start-smooth-scrolling-->
+<script type="text/javascript">
+    $(document).ready(function() {
+        var defaults = {
+            containerID: 'toTop', // fading element id
+            containerHoverID: 'toTopHover', // fading element hover id
+            scrollSpeed: 1200,
+            easingType: 'linear'
+        };
+        $().UItoTop({ easingType: 'easeOutQuart' });
+    });
+</script>
+<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+
+</body>
+</html>
